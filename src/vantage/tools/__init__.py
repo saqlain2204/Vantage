d3c2da8e62f56b6cb5c3d8b9479454cdb42efa54
Mohammed Saqlain <saqlain2204@gmail.com>
2026-04-06 21:01:37 +0530

    Added wikipedia tool

--- src/vantage/tools/__init__.py
+++ src/vantage/tools/__init__.py
@@ -1,6 +1,7 @@
 from .calculator import Calculator
 from .weather_tool import WeatherTool
 from .stack_overflow_tool import StackOverflowTool
+from .wikipedia_tool import WikipediaTool
 
-__all__ = ["Calculator", "WeatherTool", "StackOverflowTool"]
+__all__ = ["Calculator", "WeatherTool", "StackOverflowTool", "WikipediaTool"]
 
