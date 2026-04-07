23cd84b50a16493148e6007c30f9299bf0be4c4a
Mohammed Saqlain <saqlain2204@gmail.com>
2026-04-07 21:42:03 +0530

    include tool

--- src/vantage/tools/__init__.py
+++ src/vantage/tools/__init__.py
@@ -1,6 +1,8 @@
 from .calculator import Calculator
 from .weather_tool import WeatherTool
 from .stack_overflow_tool import StackOverflowTool
+from .wikipedia_tool import WikipediaTool
+from .dictionary_tool import DictionaryTool
 
-__all__ = ["Calculator", "WeatherTool", "StackOverflowTool"]
+__all__ = ["Calculator", "WeatherTool", "StackOverflowTool", "WikipediaTool", "DictionaryTool"]
 
