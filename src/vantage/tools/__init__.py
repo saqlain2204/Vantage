bf9d4575ee2aff17869e5ed2718bd9455d244b91
M Saqlain <118016760+saqlain2204@users.noreply.github.com>
2026-04-07 22:01:18 +0530

    Feat/dictionary tool (#10)
    * Added dictionary tool
    
    * include tool

--- src/vantage/tools/__init__.py
+++ src/vantage/tools/__init__.py
@@ -2,6 +2,12 @@ from .calculator import Calculator
 from .weather_tool import WeatherTool
 from .stack_overflow_tool import StackOverflowTool
 from .wikipedia_tool import WikipediaTool
+from .dictionary_tool import DictionaryTool
 
-__all__ = ["Calculator", "WeatherTool", "StackOverflowTool", "WikipediaTool"]
-
+__all__ = [
+    "Calculator",
+    "WeatherTool",
+    "StackOverflowTool",
+    "WikipediaTool",
+    "DictionaryTool",
+]
